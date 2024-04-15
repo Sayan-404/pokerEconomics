9dd7aa5da4fa814151a8a1ecd003dafdb42b0c3a
ra20r <sagnikchakraborty2002@gmail.com>
2024-04-15 22:07:17 +0530

    Debugged Game.py and added multi-hand play function

--- Game.py
+++ Game.py
@@ -9,11 +9,17 @@ class Game:
         self.number_of_players = len(players)
         self.community_cards=[]
 
-    def play(self, num):
-        # assign the number of games to be played
-        for _ in range(num):
-            self.preflop_action()
-            # Rotates list so as to put the dealer at index 0, starting with dealer at index 0
+    def flush(self):
+        self.deck.flush()
+        for player in self.players:
+            player.flush()
+        self.community_cards = []
+
+    def play(self, number_of_hands):
+        for _ in range(number_of_hands):
+            self.preflop()
+            self.flush()
+            # rotates the dealer
             self.players = self.players[1:] + self.players[:1]
 
     # increments the pot by the bet amount whenever a player bets
@@ -35,52 +41,39 @@ class Game:
                 self.gameover()
             if action == "c":
                 if bet_size >= 1:
-                    self.playerBet(current,bet_size)
+                    self.player_bet(current, bet_size)
                     print(f"{current.name} calls")
-                    players = [self.player1, self.player2]
                     bet_size = 0
                     callcount += 1
                 else:
                     callcount += 1
             if action == "r":
                 bet_size = int(input("Enter the sizing"))
-                self.playerBet(current, bet_size)
+                self.player_bet(current, bet_size)
                 print(f"{current.name} raise: {bet_size}")
             temp = current
             current = opp
             opp = temp
-        
-    #displays info at the end of a hand
-    def gameover(self):
-        print("Hand Ended")
-        for i in range(self.number_of_players):
-            print(f'player{i} stack: {self.players[i].bankroll}')
-    
-    #showdown
-    def showdown(self, players):
-        s = Showdown(self.community_cards, *players)
-        winner = players[s.winner()]
-        winner.bankroll += self.pot
-        print(f"winner: {winner.name}")
-        self.gameover()
 
     #handles the preflop action
     def preflop(self):
         print("-------PRE-FLOP------")
         for i in range(self.number_of_players):
             self.players[i].receive_card(self.deck.deal_card())
+            self.players[i].receive_card(self.deck.deal_card())
 
         bet_size = 1
 
         #blinds 
-        self.playerBet(self.players[0], 1)
-        self.playerBet(self.players[1], 2)
+        self.player_bet(self.players[0], 1)
+        self.player_bet(self.players[1], 2)
 
         #printing the cards:
         for i in range(self.number_of_players):
-            print(f"{self.players[i]}'s cards")
+            print(f"{self.players[i].name}'s cards")
             for card in self.players[i].hand:
-                print(card)
+                print(card, end=" ")
+            print()
 
         self.betting(self.players[0], self.players[1], bet_size)
         self.flop()
@@ -121,3 +114,17 @@ class Game:
 
         self.betting(self.players[1], self.players[0], bet_size)
         self.showdown(self.players)
+
+     #displays info at the end of a hand
+    def gameover(self):
+        print("Hand Ended")
+        for i in range(self.number_of_players):
+            print(f'player{i} stack: {self.players[i].bankroll}')
+    
+    #showdown
+    def showdown(self, players):
+        s = Showdown(self.community_cards, *players)
+        winner = players[s.winner()]
+        winner.bankroll += self.pot
+        print(f"winner: {winner.name}")
+        self.gameover()
