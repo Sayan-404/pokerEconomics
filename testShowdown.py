68af7c1afe3e907819073cee044f3ed98b21a4d1
ra20r <sagnikchakraborty2002@gmail.com>
2024-04-15 15:29:42 +0530

    Added new testShowdown.py

---
+++ testShowdown.py
@@ -0,0 +1,31 @@
+from Deck import Deck
+from Player import Player
+from Showdown import Showdown
+import json
+
+deck = Deck()
+deck.shuffle()
+
+players={}
+with open('players.json','r') as f:
+    players=json.load(f)
+
+# Create players
+player1 = Player(players['player1']['name'], players['player1']['bankroll'])
+player2 = Player(players['player2']['name'], players['player2']['bankroll'])
+
+for i in range(2):
+    player1.receive_card(deck.deal_card())
+    player2.receive_card(deck.deal_card())
+
+print(player1.hand)
+print(player2.hand)
+
+cc = []
+for _ in range(5):
+    cc.append(str(deck.deal_card()))
+
+print(cc)
+s = Showdown(cc, player1, player2)
+print(s)
+print(s.winner())
