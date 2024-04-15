9c27e8368bbb86d5c4aea27da51fda4118859e6b
ra20r <sagnikchakraborty2002@gmail.com>
2024-04-15 14:58:08 +0530

    Removed testshowdown.py

--- createPlayer.py
+++ createPlayer.py
@@ -1,9 +1,17 @@
 import json 
-players= {
-        'player1' : {'name':'Sayan', 'bankroll': 100},
-        'player2' : {'name':'Player 2', 'bankroll': 100}
-    }
+
+if __name__ == "__main__":
+    players = {
+            'player1' : {
+                'name':'Sayan',
+                'bankroll': 100
+                },
+            'player2' : {
+                'name':'Player 2',
+                'bankroll': 100
+                }
+        }
 
 
-with open("players.json","w") as o:
-    json.dump(players,o)
+    with open("players.json", "w") as o:
+        json.dump(players, o)
