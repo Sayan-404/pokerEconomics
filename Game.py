cf2d3046b9c1274dcc056d5498bc0031afac74ca
Sayan-404 <sayantbs@gmail.com>
2024-04-28 22:01:16 +0530

    Bugs fixed

--- Game.py
+++ Game.py
@@ -126,8 +126,12 @@ class Game:
         bet_size = 2
 
         #blinds 
-        self.player_bet(self.players[1%len(self.players)], 1)
-        self.player_bet(self.players[2%len(self.players)], 2)
+        if len(self.players) > 2:
+            self.player_bet(self.players[1%len(self.players)], 1)
+            self.player_bet(self.players[2%len(self.players)], 2)
+        else:
+            self.player_bet(self.players[0], 1)
+            self.player_bet(self.players[1], 2)
 
         print("----BLINDS-----")
         for player in self.players:
