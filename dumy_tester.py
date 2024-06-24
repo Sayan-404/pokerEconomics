1798593a87fc00e4fc3911dc875ef58e5a83ac7e
Ra20r <sagnikchakraborty2002@gmail.com>
2024-06-24 15:57:05 +0530

    added better time

--- dumy_tester.py
+++ dumy_tester.py
@@ -1,11 +1,15 @@
 from Deck import Deck
 from strats.hand_potential import potential
+import time
 d = Deck()
 d.create_deck()
 d.shuffle()
 deck = [str(card) for card in d.cards]
 hole = [deck.pop() for _ in range(2)]
 community_cards = [deck.pop() for _ in range(3)]
+a = time.time()
 p = potential(deck, hole, community_cards)
+b = time.time()
+print(b-a)
 print(p[0])
 print(p[1])
