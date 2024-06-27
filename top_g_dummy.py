ebe0fce50f49f677b84d880cd21af34471c2eca0
Sourjya Sarkar <sourjyas.work@gmail.com>
2024-06-27 13:18:22 +0530

    Evaluator implemented in cpp

---
+++ top_g_dummy.py
@@ -0,0 +1,21 @@
+from evaluator.evaluate_cards import evaluate_cards
+from itertools import combinations
+import random
+import time
+rank = "23456789TJQKA"
+suit = "csdh"
+deck = [r+s for r in rank for s in suit]
+possible_combinations = list(combinations(deck, 5))
+total_time = 0
+runs = 100000
+
+
+for _ in range(runs):
+    a = time.time()
+    t = possible_combinations[random.randint(0, len(possible_combinations)-1)]
+    _ = evaluate_cards(*t)
+    b = time.time()
+    total_time += b-a
+print(f"Total time: {total_time}")
+print(f"Average time for each process: {total_time/runs}")
+# pheval(python): .69 seconds
