d75fc4d4bd0c7af8e85f8563308f6f11aedcf484
Sourjya Sarkar <sourjyas.work@gmail.com>
2024-06-29 14:21:57 +0530

    Added batch_2 configs and minor modification to generator.py

--- configs/generator.py
+++ configs/generator.py
@@ -44,22 +44,23 @@ def generate_round_robin_strategy_configs():
     log_hands = True if input("Enter y to log individual hands: ") == "y" else False
     runs = int(input("Enter number of runs: "))
     seed = input("Enter seed if present: ")
-    batch_no = input("Enter batch number: ")
+    bankroll = input("Enter bankroll (-1 for default of 1000000000): ")
+    bankroll = bankroll if bankroll != -1 else 1000000000
     for i in range(len(strats)):
         for j in range(i+1, len(strats)):
             s1 = strats[i]
             s2 = strats[j]
-            create_config_file(f"configs/{s1}_vs_{s2}", log_hands, runs, s1, s2, seed)
+            create_config_file(f"configs/{s1}_vs_{s2}", log_hands, bankroll, runs, s1, s2, seed)
 
 
-def create_config_file(filename, log_hands, runs, strat1, strat2, seed=None):
+def create_config_file(filename, log_hands, bankroll, runs, strat1, strat2, seed=None):
     with open(f"{filename}.json", "w") as f:
         json.dump({
             "log_hands": log_hands,
             "runs": runs,
             "simulation": True,
-            "player1": {"id": strat1, "bankroll": 1500000, "strategy": strat1},
-            "player2": {"id": strat2, "bankroll": 1500000, "strategy": strat2},
+            "player1": {"id": strat1, "bankroll": int(bankroll), "strategy": strat1},
+            "player2": {"id": strat2, "bankroll": int(bankroll), "strategy": strat2},
             "seed": seed if seed else None
         }, f, indent=4)
 
