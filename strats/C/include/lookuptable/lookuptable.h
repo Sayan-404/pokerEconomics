4576165d354684c3f7aa55c7614f96f8f19f7710
Sayan-404 <sayantbs@gmail.com>
2024-07-03 22:48:19 +0530

    C program added for simplified hand potential

---
+++ strats/C/include/lookuptable/lookuptable.h
@@ -0,0 +1,13 @@
+#ifndef NEWLOOKUPTABLE_H
+#define NEWLOOKUPTABLE_H
+
+
+// int oppSearch(int hand[], int handlength);
+int pSearch(int hand[], int handlength);
+
+void freeArray();
+void assignArray();
+// void oppInsert(int hand[], int handlength, int data);
+void pInsert(int hand[], int handlength, int data);
+
+#endif
