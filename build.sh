e58cd4b52607dec30fe10208580034dd3a481dcd
Ra20r <sagnikchakraborty2002@gmail.com>
2024-07-09 15:40:45 +0530

    updated build.sh

--- build.sh
+++ build.sh
@@ -4,15 +4,9 @@ cd build
 cmake ..
 make pheval
 cd ..
-g++ -fPIC --shared -std=c++11 -I include/ wrapper.cpp build/libpheval.so.0.6.0 -o wrapper.so
-
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    g++ -fPIC --shared -std=c++11 -I include/ wrapper.cpp build/libpheval.0.6.0.dylib -o wrapper.so
+else
+    g++ -fPIC --shared -std=c++11 -I include/ wrapper.cpp build/libpheval.so.0.6.0 -o wrapper.so
+fi
 cd ..
-
-echo "Building simplified_hand_potential"
-cd poker_metrics/simplified_hand_potential
-mkdir -p build
-cd build
-cmake ..
-make pheval
-cd ..
-gcc -fPIC --shared -I include/ simplified_hand_potential.c build/libpheval.so.0.6.0 -o potential.so
