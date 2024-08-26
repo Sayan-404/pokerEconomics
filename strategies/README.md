e8ad993f473fa5911c979e5145edec953af64431
orientsvictor <sourjyas.work@gmail.com>
2024-08-26 08:51:53 +0530

    documented strategies

--- strategies/README.md
+++ strategies/README.md
@@ -1,147 +1,59 @@
 # Strategies
 
-## Types
+There are two types of strategies,
 
-There are two types of strategies. Action based and rational strategies.
-
-1. Action based strategies takes decision on a specific pattern, inspired from Axelrod's work.
-2. Rational strategies considers the unknown or imperfect information available in the game and then takes decision.
-
-## Action Based Strategies
-
-These strategies takes decision without any consideration for information.
-
-### alwaysFold
-
-Just fold every single time.
-
-### Cooperative
-
-Always checks/calls.
-
-### Defective
-
-Always raises/bets.
-
-### Grim
-
-Starts by checking/calling. However, if opponent even raises/bets once, it always raises/bets after that.
-
-#### Tit For Tat
-
-Replicates the opponents last action else checks/calls.
-
-#### Generous Tit For Tat
-
-Replicates the opponents last action else checks/calls. However, if the opponent raised against it, it may randomly choose to pardon that and check.
-
-#### Random (With Fold)
-
-Takes a random valid move.
-
-#### Random (No Fold)
-
-Takes a random valid move apart from fold.
+1. **Action:** These strategies are trivial in nature and are very basic & simple.
+2. **Rational:** These strategies have a sense of rationality among them.
 
 ## Rational Strategies
 
-Rational Strategies takes decision based upon the available imperfect information.
+### Strategy.py
 
-### Basic Setup
+This is the main object that gives the strategy a sense of rationality. Here's how it works.
 
-1. All the strategies are the child class of the `Strategy` parent class in `Strategy.py`.
-2. They share the same rationality (determined by the signal function).
-3. Point to note over here is that their decide function is different.
-
-Basically, this setup helps us to study the decision taken by rational agents if they have the same intelligence or knowledge.
-
-### The Strategies
-
-#### Ideal
-
-These strategies do not deviate from the overall rationality and takes the ideal decision given by the signal function in all scenarios. For example,
-
-```algorithm
-    SignalRange > 0.85:
-        Bet/Raise
-    SignalRange > 0.35:
-        Check/Call
-    Else:
-        Fold
-```
-
-**Note:** This is just an overview of what happens inside the ideal strategy.
-
-Now these strategies are itself divided into 5 strategies, indicative of their tightness, ranging from `very_loose` to `very_tight` with `balanced` being the one with moderate tightness.
-
-In this scenario, the 5 strategies has different brackets for `signalRange` to make the decision.
-
-#### Changing_Styles
-
-This strategy, given that it is a scenario to not fold (surrender), takes random move.
-
-It can take any of the following move,
-
-1. Randomly make a prodigal (bet/raise) or a frugal (check/call) move.
-2. Randomly overbet, underbet or equally bet.
-3. Randomly changes it's tightness.
-
-#### Bluff
-
-The bluff has the following characteristics.
-
-1. Given that it is not a scenario to fold, it plays as it should while semi-bluffing with a good hand.
-2. If it is a scenario to fold, it bluffs.
-
-**How does it makes the decision to bluff?** It calculates the probability that an opponent folds when the strategy has raised earlier. If the probability is greater than 0.55, then it bluffs aggressively. Otherwise it still bluffs but in a conservative fashion and randomly.
-
-#### Check_Raise
-
-This strategy has the following characteristics given that it has an exceptional hand.
-
-1. If the probability that opponent raises is above than 0.55, it checks/calls.
-2. When the opponent raises, it raises aggressively to stuck opponent bets.
-
-However, if it does not have an exceptional hand, it will not deviate from the ideal behaviour.
-
-#### Slowplaying
-
-This strategy has the following characteristics given that it has an exceptional hand.
-
-1. Plays the first round where the emergence of an exceptional hand occurred very weakly (check/call only).
-2. If the hand is still exception in the next round (which it should be most of times), it plays according to the ideal behaviour (whish is to raise/bet) with the option to bet a bit aggresively.
-3. Else it plays according to the ideal behaviour.
-
-## Parameters
-### non-bluffing strategies
-
-| Name | r_shift | l_shift | risk |
-| ---- | ---- | ---- | ---- |
-| balanced | 0 | 0 | 0 |
-| balanced_low_risk | 0 | 0 | 0.2 |
-| balanced_medium_risk | 0 | 0 | 0.6 |
-| belanced_high_risk | 0 | 0 | 1 |
-| loose | 0.5 | 0 | 0 |
-| loose_low_risk | 0.5 | 0 | 0.2 |
-| loose_medium_risk | 0.5 | 0 | 0.6 |
-| loose_high_risk | 0.5 | 0 | 1 |
-| tight | 0 | 0.5 | 0 |
-| tight_low_risk | 0 | 0.5 | 0.2 |
-| tight_medium_risk | 0 | 0.5 | 0.6 |
-| tight_high_risk | 0 | 0.5 | 1 |
-| very_loose | 1 | 0 | 0 |
-| very_loose_low_risk | 1 | 0 | 0.2 |
-| very_loose_medium_risk | 1 | 0 | 0.6 |
-| very_loose_high_risk | 1 | 0 | 1 |
-| very_tight | 0 | 1 | 0 |
-| very_tight_low_risk | 0 | 1 | 0.2 |
-| very_tight_medium_risk | 0 | 1 | 0.6 |
-| very_tight_high_risk | 0 | 1 | 1 |
-
-### bluffing strategies (bluff == True)
-
-| Name | r_shift | l_shift | risk |
-| ---- | ---- | ---- | ---- |
-| bluff | 0 | 0 | 0 |
-| bluff_1 | 0.5 | 0 | 0.5 |
-| bluff_2 | 1 | 0 | 1 |
+1. **Factoring in basic information:**
+   When the decide function is called before taking a decision (prodigal or frugal), if new information is present as a parameter (or not an empty dictionary), the following are internalised as class attributes.
+   - `seed` for the random functions.
+   - `holeCards` & `communityCards`.
+   - Current `round`.
+   - `bigBlind` required for setting initial pot limit.
+   - `callValue` required for finding pot odds.
+   - `playerBetAmt` is the amount player have bet till that point.
+   - `pot` current pot size.
+2. **Setting initial pot:**
+   Initial pot is the amount of that pot which occurs in the beginning of a specific round (pre-flop/flop/turn/river). This is done by calling the `self.setInitialPot()` method. It is to be calculated in two ways.
+   1. **If round == 0:**
+      When `round == 0`, the initial pot means the 2BB or 2 big blinds (actually it is 1SB + 1BB but for simplicity we will consider the pot to be taken in multiple of big blinds).
+   2. **If round != 0:**
+      Ideally in this scenario it is the value `pot - callValue` converted to nearest big blind but however this code is commented as it will not be required and instead a default of -1 will be the value.
+      Note that this is only applicable for heads-up games.
+3. **Setting round limiter:**
+   1. If `round == 0` and `iniLimitMultiplier` is an integer, then `limit` (the maximum a player can bet in a round) is capped at `iniLimitMultiplier * initialPot`.
+   2. Otherwise, it will set the limit with the `defaultLimit`.
+4. **Reason the decision:**
+   After preparation of the information, the `reason` method will be called to actually figure out more about the given information. It calculates the followings:
+   1. privateValue/hand_strength/hs (`self.hs`).
+   2. Inverts the hand_strength if `bluff > 0` and `self.hs < 0.5`.
+   3. potential/sp (0 if `round in [0, 3]`)
+   4. pot_odds/po
+   5. `self.ll = self.po/(1 - self.po)`.
+   6. `self.ul = self.sp + self.hs + self.risk`
+   7. `t_determiner = self.ul - self.ll`
+5. **Figure out appropriate move & bet size:**
+   The appropriate move will be determined with `t_determiner`. Here's how it works,
+   1. **`t_determiner <= 0:`** Out of money scenario - explicitly check/fold by setting `self.betAmt` as -1.
+   2. **`t_determiner == 0:`** In this point strategy is in balanced position. Only call/check by returning 0.
+   3. **`t_determiner > 0:`** In the money scenario. Get the odds `r` by calling the `odds` function in the `math_utils.py` and set the monetary value `pot * r` as bet amount `self.betAmt`.
+6. **Limit the bet amount:**
+   Limit the bet amount to the value of `self.limit` if bet amount is greater than limit by calling the `limiter` method.
+   - If the bet amount is explicitly -1 or 0 (check/fold/call), then the `limiter` method passes.
+   - Else,
+     - It calculates the total current bet `tcb = betAmt + playerBetAmt` which is the bet size that player raised/made till this point.
+     - If the `tcb > limit`,
+       - It calculates the required bet `req = callValue + playerBetAmt` to stay in the game.
+       - If `req > limit` then limit has been passed and makes the player forcefully call by setting `betAmt` to 0.
+       - Else it sets the `betAmt = limit - playerBetAmt`.
+7. **Convert bet amount to to blinds:**
+   The `toBlinds` method is then called to convert the bet amount to to the nearest multiple of big blind (if the bet amount is not equal to -1 or 0, indicating check/fold/call).
+8. **Set the appropriate move:**
+   Finally, an appropriate move is decided by calling the `setMove` method.
--- strategies/Strategy.py
+++ strategies/Strategy.py
@@ -7,14 +7,36 @@ import random
 class Strategy:
     def __init__(self):
         """
-            Initialises all the required variables for easy access by children classes.
+            Initializes all the required variables for easy access by children classes.
+            
+            This constructor initializes various attributes of the `Strategy` class, including:
+            
+            - `eval`: A boolean flag indicating whether the strategy is being used for automated data generation.
+            - `iniLimitMultiplier`: A variable used for setting the initial limit.
+            - `seed`: The seed value for the strategy.
+            - `information`: A dictionary containing game information.
+            - `holeCards`: The player's hole cards.
+            - `communityCards`: The community cards.
+            - `round`: The current round of the game.
+            - `prevActionRound`: The previous action round.
+            - `callValue`: The current call value.
+            - `playerBetAmt`: The total amount the player has bet in the current hand.
+            - `pot`: The current pot size.
+            - `initialPot`: The initial pot size, used for limiting the pot.
+            - `betAmt`: The additional amount the player will bet or raise.
+            - `bigBlind`: The big blind value.
+            - `hs`, `sp`, `po`, `t_determiner`, `effectivePotential`: Metrics used for decision making and placing bets.
+            - `r_shift`, `l_shift`, `risk`: Numbers defining the "prodigalness", "frugalness", and risk capacity of the strategy.
+            - `bluff`: A flag indicating whether the player should bluff.
+            - `move`: The move the player will make.
+            - `defaultLimit`: The default limit for the strategy.
         """
+        
         # If eval True then strategy is being used for automated data generation
         self.eval = False
 
         # Variables for initial limit
-        self.iniLimit = False
-        self.iniLimitMultiplier = 0
+        self.iniLimitMultiplier = -1
 
         self.seed = None
 
@@ -58,11 +80,19 @@ class Strategy:
 
         self.move = ()
 
-        self.limit = 100000
+        # self.defaultLimit = 100000
 
     def initialise(self, information):
         """
-            Takes the information state and initialises all the variables before making an action.
+            Initializes the strategy with the provided game information.
+    
+            This method is responsible for setting up the initial state of the strategy based on the information provided in the `information` dictionary. It extracts and sets various attributes of the strategy, such as the player's hole cards, the community cards, the current round, the big blind, the call value, the player's bet amount, the pot size, and the initial pot size.
+    
+            If this is the pre-flop round and the `iniLimitMultiplier` is greater than 0, the method sets the `limit` attribute based on the initial pot size. Otherwise, it sets the `limit` to the `defaultLimit`.
+    
+            After setting the initial state, the method calls several other methods (`reason()`, `setBet()`, `limiter()`, `toBlinds()`, `setMove()`) to further process the game information and determine the strategy's move.
+    
+            Finally, the method checks if the calculated bet amount (`self.betAmt`) is greater than the `limit`, and raises an exception if it is.
         """
 
         self.information = information
@@ -77,32 +107,47 @@ class Strategy:
 
         self.callValue = information["call_value"]
 
+        # It is the amount player has bet till now
         self.playerBetAmt = information["player"]["betamt"]
+
         self.pot = information["pot"]
 
         self.setInitialPot()
 
-        if self.iniLimit:
-            if self.round == 0:
-                self.limit = self.iniLimitMultiplier*self.initialPot
-            else:
-                self.limit = 10000
+        # If this is the pre-flop then set initial limit
+        if (self.round == 0) and (self.iniLimitMultiplier > 0):
+            self.limit = self.iniLimitMultiplier*self.initialPot
+        else:
+            # Else set default limit as limit
+            self.limit = self.defaultLimit
 
+        # Check the README for insight into the following methods
         self.reason()
         self.setBet()
         self.limiter()
         self.toBlinds()
         self.setMove()
 
+        # This statement checks for error
+        # NOTE: In some rare cases the betAmt may be greater by 1BB or a very small size
         if (self.betAmt > self.limit):
             raise Exception(f"{self.betAmt} {self.limit}")
 
         self.prevActionRound = self.round
 
     def decide(self, information):
+        """
+            Decides the action to take based on the current game state.
+            
+            This function is responsible for initializing the game state and returning the appropriate move to make. If the `eval` attribute is set to `True`, the function will initialize the game state using the provided `information` dictionary and return the calculated move. If `eval` is `False`, a `NotImplementedError` is raised indicating that the `decide` function has not been implemented for the current strategy.
+        """
+
         # This function should be `initialised` so that it can use class variables
         if self.eval:
+            # self.eval is True when the strategies are created by engine
+            # Simply, it is when the strategy does not have a python file associated with it
             if information == {}:
+                # Pass if there is no information
                 pass
             else:
                 self.initialise(information)
@@ -112,6 +157,17 @@ class Strategy:
                 f"The decide function is not implemented by {self.strategy}")
 
     def reason(self):
+        """
+            Calculates the necessary values for determining the betting strategy.
+            
+            This method calculates various values that are used to determine the betting strategy, including:
+            - The hand strength (`self.hs`) using the `privateValue` function.
+            - Whether a bluff should be attempted (`self.bluffer`) if `self.bluff` is greater than 0.
+            - The pot odds (`self.po`) based on the call value and the pot size.
+            - The lower limit (`self.ll`) and upper limit (`self.ul`) of the betting range, which are derived from the pot odds and the potential (`self.sp`).
+            - The t-determiner (`self.t_determiner`) which is the difference between the upper and lower limits, and is used to determine the betting strategy.
+        """
+                
         self.hs = privateValue(self.holeCards, self.communityCards)
 
         # Bluff if true
@@ -119,42 +175,55 @@ class Strategy:
             self.bluffer()
 
         self.sp = potential(self.holeCards, self.communityCards) if self.round in [1, 2] else 0
-        self.effectivePotential = self.hs if self.round in [0, 3] else self.sp
 
         self.po = (self.callValue/(self.pot + self.callValue))
 
         self.ll = self.po/(1 - self.po)
-        self.ul2 = self.sp + self.hs + self.risk
+        self.ul = self.sp + self.hs + self.risk
 
-        self.t_determiner = self.ul2 - self.ll
+        self.t_determiner = self.ul - self.ll
 
     def setBet(self):
-        """Decides bet based on the mathematical logic"""
+        """
+            Sets the bet amount based on the calculated t-determiner value.
+
+            - If the t-determiner is greater than 0, the strategy is considered "in the money" and the bet amount is calculated as a fraction of the pot based on the odds function. 
+            - If the t-determiner is 0, the strategy is considered in a "balanced position" and the bet amount is set to 0 (call/check).
+            - If the t-determiner is less than or equal to 0, the strategy is considered "out of money" and the bet amount is set to -1 (check/fold).
+        """
 
         if self.t_determiner > 0:
-            # When t' > 0 then strategy is in the money
+            # When t > 0 then strategy is in the money
 
             # Get odds from the odds function and then derive the bet amount
             # The odd is decided randomly from player's playing range
 
-            self.r = odds(self.ll, self.ul2, self.hs, self.risk,
+            self.r = odds(self.ll, self.ul, self.hs, self.risk,
                           self.l_shift, self.r_shift, self.seed)
 
             self.monValue = round(self.pot*self.r)
             self.betAmt = self.monValue
 
         elif self.t_determiner == 0:
-            # When t' == 0 then strategy is in balanced position
+            # When t == 0 then strategy is in balanced position
             # Only Call/Check (return 0 as bet amount)
             self.betAmt = 0
 
         elif self.t_determiner <= 0:
-            # When t' <= 0 then strategy is out of money
+            # When t <= 0 then strategy is out of money
             # Explicitly check/fold (return -1 as bet amount)
             self.betAmt = -1
 
     def setMove(self):
-        """Sets an appropriate move based on bet amount"""
+        """
+            Determines the move to make based on the calculated bet amount.
+
+            - If the bet amount is -1, it means a check/fold is explicitly requested, so the move is set accordingly.
+            - If the bet amount is 0 or equal to the call value, a frugal move is made.
+            - If the bet amount is greater than the call value, a prodigal move is made.
+            - If the bet amount is less than the call value, an exception is raised as this scenario should not occur.
+            - If the bet amount is invalid, an exception is raised.
+        """
 
         if self.betAmt == -1:
             # Check/Fold when explicitly -1
@@ -181,21 +250,40 @@ class Strategy:
             raise Exception(f"Invalid bet amount: {self.betAmt}")
 
     def setInitialPot(self):
-        """Sets initial pot as round's first pot value"""
+        """
+            Sets the initial pot value for the current game round.
+
+            This method is only applicable for heads-up games. If the current round is 0 and the previous action round is not 0, the initial pot is set to 2 times the big blind. Otherwise, the initial pot is set to -1.
+
+            The commented-out code shows a generalised way to calculate the initial pot.
+        """
 
         # Only applicable for heads-up
         if (self.round == 0) and ((self.prevActionRound != 0)):
-            initialPot = self.bigBlind + (self.bigBlind/2)
-            self.initialPot = round(initialPot/self.bigBlind) * self.bigBlind
+            self.initialPot = 2 * self.bigBlind
 
-        elif self.prevActionRound < self.round:
-            initialPot = (self.pot - self.callValue)
+            # Ideally
 
-            # Converting to blinds
-            self.initialPot = round(initialPot/self.bigBlind) * self.bigBlind
+            # initialPot = self.bigBlind + (self.bigBlind/2)
+            # self.initialPot = round(initialPot/self.bigBlind) * self.bigBlind
+
+        else:
+            self.initialPot = -1
+
+        # Ideally
+
+        # elif self.prevActionRound < self.round:
+        #     initialPot = (self.pot - self.callValue)
+
+        #     # Converting to blinds
+        #     self.initialPot = round(initialPot/self.bigBlind) * self.bigBlind
 
     def limiter(self):
-        """Limit the bet amount to 3 times initial pot"""
+        """
+            Limits the bet amount based on the player's total bet amount and the specified limit.
+
+            If the total bet amount (including the player's bet) exceeds the limit, the bet amount is adjusted to the maximum allowed within the limit. If the call value plus the player's bet amount exceeds the limit, the bet amount is set to 0 to force a call.
+        """
 
         if self.betAmt in [-1, 0]:
             # Ignore limit when explicitly check or fold
@@ -214,8 +302,14 @@ class Strategy:
                 else:
                     self.betAmt = self.limit - self.playerBetAmt
 
-    def toBlinds(self, amt=-1):
-        """Convert the monetary value to nearest big blind multiple"""
+    def toBlinds(self):
+        """
+            Converts the monetary value to the nearest big blind multiple.
+            
+            - If the bet amount is explicitly a check/fold or call scenario, this method does nothing.
+            - Otherwise, it calculates the bet amount relative to the call value, rounds it to the nearest big blind multiple, and updates the bet amount.
+        """
+        
         if self.betAmt in [-1, 0]:
             # Explicitly check/fold or call scenario
             pass
@@ -225,9 +319,14 @@ class Strategy:
             self.betAmt = bet + self.callValue
 
     def bluffer(self):
-        # Inverses a strategy's hand strength to turn weak hand to strong
+        """
+            Inverses a strategy's hand strength to turn weak hand to strong. 
+            If the hand strength is less than 0.5, there is a 1 in `self.bluff` chance that the hand strength will be set to 1 minus the original hand strength.
+        """
 
-        if self.hs < 0.55:
+        # Only applicable if hand strength is less than 0.5
+        if self.hs < 0.5:
+            # 1 in `self.bluff` chance to inverse hand strength
             if 1 == random.randint(1, self.bluff):
                 self.hs = 1 - self.hs
 
